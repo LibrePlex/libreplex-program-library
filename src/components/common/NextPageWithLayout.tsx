4b4c9910004662770ac22db75a37faba871ae283
Tom Nygren <tomnygren@guzz.com>
2023-05-25 02:59:18 +0100

    Add nextjs + badly broken wallet connector

---
+++ src/components/common/NextPageWithLayout.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
