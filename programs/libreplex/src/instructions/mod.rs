2780c3c9ba45f3529e3256868d86b7c18dd351e8
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2023-06-03 15:43:27 +0100

    Basic permissions

--- programs/libreplex/src/instructions/mod.rs
+++ programs/libreplex/src/instructions/mod.rs
@@ -2,8 +2,10 @@ pub mod create_collection_data;
 pub mod create_metadata;
 pub mod delete_collection_data;
 pub mod delete_metadata;
+pub mod edit_permissions;
 
 pub use create_collection_data::*;
 pub use create_metadata::*;
 pub use delete_collection_data::*;
 pub use delete_metadata::*;
+pub use edit_permissions::*;
