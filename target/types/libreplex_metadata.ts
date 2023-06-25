9fc1e16f35526bfad9bd865acc38d549f40448e7
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2023-06-25 18:21:58 +0100

    Add marketplace draft

--- target/types/libreplex_metadata.ts
+++ target/types/libreplex_metadata.ts
@@ -1,5 +1,5 @@
 export type LibreplexMetadata = {
-  "version": "0.4.1",
+  "version": "0.4.3",
   "name": "libreplex_metadata",
   "instructions": [
     {
@@ -1834,7 +1834,7 @@ export type LibreplexMetadata = {
 };
 
 export const IDL: LibreplexMetadata = {
-  "version": "0.4.1",
+  "version": "0.4.3",
   "name": "libreplex_metadata",
   "instructions": [
     {
