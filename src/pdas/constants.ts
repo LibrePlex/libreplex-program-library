e8205b80e76a8ef9a115eefea22f8cfe99e2653f
FallingHazard <FallingHazard@users.noreply.github.com>
2023-08-31 17:53:48 +0200

    Remove src folder

--- src/pdas/constants.ts
+++
@@ -1,5 +0,0 @@
-export const GROUP = "group";
-export const PERMISSIONS = "permissions";
-export const METADATA = "metadata";
-export const METADATA_EXTENSION = "metadata_extension";
-export const LISTING = "listing";
