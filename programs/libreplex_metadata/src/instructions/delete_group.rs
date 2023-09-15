a066f3a18622db453c99b76123e649907fc6bd48
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2023-09-15 18:31:33 +0100

    Add delete group endpoint

---
+++ programs/libreplex_metadata/src/instructions/delete_group.rs
@@ -0,0 +1,37 @@
+use anchor_lang::prelude::*;
+use crate::state::Group;
+
+
+#[event]
+pub struct GroupEventDelete {
+    pub authority: Pubkey,
+    pub name: String,
+    pub id: Pubkey,    
+}
+
+#[derive(Accounts)]
+pub struct DeleteGroup<'info> {
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(mut, 
+        close = authority,
+        constraint = group.update_authority == authority.key())]
+    pub group: Box<Account<'info, Group>>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<DeleteGroup>
+) -> anchor_lang::Result<()> {
+
+    let group = &ctx.accounts.group;
+    emit!(GroupEventDelete{
+        authority: ctx.accounts.authority.key(),
+        name: group.name.clone(),
+        id: group.key()
+    });
+
+    Ok(())
+}
